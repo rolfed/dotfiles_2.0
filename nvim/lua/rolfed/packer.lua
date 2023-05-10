2ea6522d3fdb46f9a108e8d0c42364f609a5a6a8
Danniel Rolfe <talkto@dannielrolfe.com>
2023-05-10 09:19:56 -0700

    Add lsp

--- nvim/lua/rolfed/packer.lua
+++ nvim/lua/rolfed/packer.lua
@@ -23,8 +23,31 @@ return require('packer').startup(function(use)
 
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}) 
 	use('nvim-treesitter/playground')
-	use('theprimeagen/harpoon')
+ 	use('theprimeagen/harpoon')
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
+
+	-- LSP
+	use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+	  -- LSP Support
+	  {'neovim/nvim-lspconfig'},             -- Required
+	  {                                      -- Optional
+	  'williamboman/mason.nvim',
+	  run = function()
+		  pcall(vim.cmd, 'MasonUpdate')
+	  end,
+  },
+  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+  -- Autocompletion
+  {'hrsh7th/nvim-cmp'},     -- Required
+  {'hrsh7th/cmp-nvim-lsp'}, -- Required
+  {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
+
 
 end)
