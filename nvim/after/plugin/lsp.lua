66c36fbd3d6b12e8a4e687d6fbc212764ced5ff1
Danniel Rolfe <talkto@dannielrolfe.com>
2023-05-10 09:28:53 -0700

    Add lsp configuration

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -7,4 +7,9 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+lsp.ensure_installed({	
+	'tsserver',
+	'eslint',
+})
+
 lsp.setup()
