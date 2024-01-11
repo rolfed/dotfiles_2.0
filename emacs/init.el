476251b2d9b605861095d8117006a405557b56a0
Danniel Rolfe <danniel.rolfe@experian.com>
2024-01-11 15:33:46 -0800

    add ido, better buffer management and avy

--- emacs/init.el
+++ emacs/init.el
@@ -37,7 +37,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(beacon catppuccin-theme which-key)))
+ '(package-selected-packages '(smex ido-vertical-mode beacon catppuccin-theme which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
