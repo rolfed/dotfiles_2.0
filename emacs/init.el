58e8106a467e38d13a2de6fa27247234e7872fc7
Danniel Rolfe <talkto@dannielrolfe.com>
2024-01-14 15:16:58 -0800

    Install treemacs and map p v for project view

--- emacs/init.el
+++ emacs/init.el
@@ -38,7 +38,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(smex ido-vertical-mode beacon catppuccin-theme which-key)))
+ '(package-selected-packages
+   '(treemacs-evil treemacs company smex ido-vertical-mode beacon catppuccin-theme which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
