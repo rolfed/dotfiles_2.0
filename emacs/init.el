7d213b646d8872579a73f32546e04a9dde77ce86
Danniel Rolfe <talkto@dannielrolfe.com>
2024-01-15 14:37:01 -0800

    wip

--- emacs/init.el
+++ emacs/init.el
@@ -45,4 +45,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-block ((t (:background "#f3bce6")))))
+ '(org-block ((t (:background "#393c4d")))))
